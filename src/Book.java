ef90cb3e15d252a6ec9e927c3cae759824174f08
Rabota <wulf1205@gmail.com>
2023-10-26 21:12:21 +0700

    больше не знаю как

--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,17 @@
 public class Book {
     private String title;
-    private Author author;
+    private Author authorName;
     private int age;
-    public Book(String title,Author author, int age) {
+    public Book(String title,Author authorName, int age) {
         this.title = title;
+        this.authorName = authorName;
         this.age = age;
     }
     public String getTitle() {
         return this.title;
+    }
+    public Author getAuthorName() {
+        return this.authorName;
     }
 
     public int getAge() {
